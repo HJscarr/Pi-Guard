f7987037476b0318507177f2cc0679647060b8fc
James Scarr <51050970+HJscarr@users.noreply.github.com>
2023-12-31 11:44:32 +0000

    Update record-vid-easy.py

--- record-vid-easy.py
+++ record-vid-easy.py
@@ -1,13 +1,15 @@
 from picamera import PiCamera
 from datetime import datetime
+import subprocess
 
 # Record duration
 duration   = 10
 frame_rate = 12
 
 # Generate a unique file name with timestamp
-timestamp = datetime.now().strftime("%d-%m-%H:%M")
-video_file = f"/home/pi/video_{timestamp}.h264"
+timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M")
+h264_video_file = f"/home/js17/video_{timestamp}.h264"
+mp4_video_file = f"/home/js17/video_{timestamp}.mp4"
 
 camera = PiCamera()
 camera.resolution = (640, 480)
@@ -16,9 +18,16 @@ camera.framerate = frame_rate
 # Record Video
 print("Starting video recording...")
 
-camera.start_recording(video_file)
+camera.start_recording(h264_video_file)
 camera.wait_recording(duration)
 camera.stop_recording()
 camera.close()
 
-print(f"Video recorded at: {video_file}")
+print(f"Video recorded at: {h264_video_file}")
+
+# Convert to MP4
+print("Converting to MP4 format...")
+command = f"ffmpeg -framerate {frame_rate} -i {h264_video_file} -c copy {mp4_video_file}"
+subprocess.run(command, shell=True)
+
+print(f"Video converted and saved as: {mp4_video_file}")
